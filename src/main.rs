9a8b70592f13a04b105856013b360fa0e492ea49
tylr <tylr@MBP.local>
2022-05-23 08:31:03 -0700

    guessing game complete

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,39 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
     println!("Hello...");
     println!("Lets play a guessing game!");
-    println!("The number is in betwee 1 and 100..");
-    println!("Enter your first guess: ");
-    let mut guess = String::new();
+    println!("The number is in between 1 and 100..");
+    println!("The secret number is: {}", secret_number);
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    loop {
+        let mut guess = String::new();
 
-    println!("Your guess was: {}", guess);
+        println!("Enter a guess: ");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        println!("Your guess was: {}", guess);
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num, 
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("To small!"),
+            Ordering::Greater => println!("To big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            } 
+        }
+    }
+    
 
 }
